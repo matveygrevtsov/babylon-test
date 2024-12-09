ed2e129d6aa8141cbc17e29e61c7baac6df56351
matveygrevtsov <matveygrevcov@gmail.com>
2024-12-09 09:25:14 +0300

    Updates

---
+++ 144.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbabylon_test=self.webpackChunkbabylon_test||[]).push([[144],{144:(e,t,l)=>{var n=l(540);l(338).createRoot(document.getElementById("root")).render(n.createElement(n.StrictMode,null,n.createElement((function(){return n.createElement("h1",null,"Hello, world!")}),null)))}}]);
--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Scene } from "./components/Scene/Scene";
-
-export const App = () => {
-  return <Scene />;
-};
