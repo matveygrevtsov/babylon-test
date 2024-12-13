fc46a4a69f78a28e431f9a322e3f64a461bbb22b
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2024-12-13 22:16:39 +0300

    fix camera (#17)
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/components/Example1/useScene.ts
+++ src/components/Example1/useScene.ts
@@ -5,9 +5,9 @@ import {
   Vector3,
   HemisphericLight,
   MeshBuilder,
-  FreeCamera,
   StandardMaterial,
   Color3,
+  ArcRotateCamera,
 } from "@babylonjs/core";
 import { Character } from "./Character/Character";
 
@@ -18,42 +18,50 @@ export const useScene = () => {
     const canvas = ref.current;
     if (!canvas) return;
 
+    canvas.addEventListener("click", () => {
+      canvas.requestPointerLock();
+    });
+
     const engine = new Engine(canvas, true, {
       preserveDrawingBuffer: true,
       stencil: true,
     });
 
-    const scene = (() => {
-      // Creates a basic Babylon Scene object
-      const scene = new Scene(engine);
-      // Creates and positions a free camera
-      const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-      // Targets the camera to scene origin
-      camera.setTarget(Vector3.Zero());
-      // Attaches the camera to the canvas
-      camera.attachControl(canvas, true);
-      // Creates a light, aiming 0,1,0
-      const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-      // Dim the light a small amount 0 - 1
-      light.intensity = 0.7;
-      // Built-in 'sphere' shape.
+    // Creates a basic Babylon Scene object
+    const scene = new Scene(engine);
+    // Creates and positions a free camera
+    const camera = new ArcRotateCamera(
+      "Camera",
+      -Math.PI / 2,
+      Math.PI / 4,
+      10,
+      new Vector3(0, 0, 0),
+      scene
+    );
+    // Targets the camera to scene origin
+    camera.setTarget(Vector3.Zero());
+    // Attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+    // Creates a light, aiming 0,1,0
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    // Dim the light a small amount 0 - 1
+    light.intensity = 0.7;
+    // Built-in 'sphere' shape.
 
-      const character = new Character({
-        scene,
-      });
+    const character = new Character({
+      scene,
+      camera,
+    });
 
-      // Пол (земля).
-      const ground = MeshBuilder.CreateGround(
-        "ground",
-        { width: 6, height: 6 },
-        scene
-      );
-      const groundMaterial = new StandardMaterial("groundMaterial", scene);
-      groundMaterial.diffuseColor = Color3.FromHexString("#97ae3b");
-      ground.material = groundMaterial;
-
-      return scene;
-    })();
+    // Пол (земля).
+    const ground = MeshBuilder.CreateGround(
+      "ground",
+      { width: 6, height: 6 },
+      scene
+    );
+    const groundMaterial = new StandardMaterial("groundMaterial", scene);
+    groundMaterial.diffuseColor = Color3.FromHexString("#97ae3b");
+    ground.material = groundMaterial;
 
     engine.runRenderLoop(() => {
       scene.render();
