ffb6a7b686a8856cd76163ad1656eeea48006330
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2024-12-21 23:02:16 +0300

    fix example 2 (#28)
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/components/Example2/useScene.ts
+++ src/components/Example2/useScene.ts
@@ -8,8 +8,24 @@ import {
   StandardMaterial,
   Color3,
   ArcRotateCamera,
-  PhysicsImpostor,
+  CannonJSPlugin,
 } from "@babylonjs/core";
+import { Body, Box, Quaternion, Vec3 } from "cannon-es";
+
+const mapCannonQuaternionToBabylonRotationVector = (
+  quaternion: Quaternion,
+): Vector3 => {
+  const { x, y, z, w } = quaternion;
+
+  const rotationX = Math.atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y));
+  const rotationY = Math.asin(2 * (w * y - z * x));
+  const rotationZ = Math.atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z));
+
+  return new Vector3(rotationX, rotationY, rotationZ);
+};
+
+const CUBE_SIZE = 1;
+const GROUND_SIZE = 6;
 
 export const useScene = () => {
   const ref = useRef<HTMLCanvasElement>(null);
@@ -29,7 +45,12 @@ export const useScene = () => {
 
     // Creates a basic Babylon Scene object
     const scene = new Scene(engine);
-    scene.enablePhysics();
+
+    // Создаем физический мир с помощью Cannon.js
+    const gravity = new Vector3(0, -9.81, 0);
+    const physicsPlugin = new CannonJSPlugin();
+    scene.enablePhysics(gravity, physicsPlugin);
+
     // Creates and positions a free camera
     const camera = new ArcRotateCamera(
       "Camera",
@@ -51,34 +72,95 @@ export const useScene = () => {
     light.intensity = 0.7;
     // Built-in 'sphere' shape.
 
-    const sphere = MeshBuilder.CreateSphere(
-      "sphere",
-      { diameter: 2, segments: 16 },
-      scene,
+    // Создаем куб№1
+    const box1 = MeshBuilder.CreateBox("box", { size: CUBE_SIZE }, scene);
+
+    // Добавляем физику к кубу№1
+    const boxBody1 = new Body({
+      mass: 1,
+      position: new Vec3(0, 6, 0),
+    });
+    const boxShape1 = new Box(
+      new Vec3(CUBE_SIZE / 2, CUBE_SIZE / 2, CUBE_SIZE / 2),
     );
-    sphere.position.y = 3; // Поднимаем сферу над "землей"
-    sphere.physicsImpostor = new PhysicsImpostor(
-      sphere,
-      PhysicsImpostor.SphereImpostor,
-      { mass: 1, restitution: 0.3 },
-      scene,
+    boxBody1.addShape(boxShape1);
+    physicsPlugin.world.addBody(boxBody1);
+
+    // Создаем куб№2
+    const box2 = MeshBuilder.CreateBox("box", { size: CUBE_SIZE }, scene);
+
+    // Добавляем физику к кубу№2
+    const boxBody2 = new Body({
+      mass: 0,
+      position: new Vec3(
+        (0.75 * CUBE_SIZE) / 2,
+        CUBE_SIZE / 2,
+        0.75 * CUBE_SIZE,
+      ),
+    });
+    const boxShape2 = new Box(
+      new Vec3(CUBE_SIZE / 2, CUBE_SIZE / 2, CUBE_SIZE / 2),
     );
+    boxBody2.addShape(boxShape2);
+    physicsPlugin.world.addBody(boxBody2);
 
     // Пол (земля).
     const ground = MeshBuilder.CreateGround(
       "ground",
-      { width: 6, height: 6 },
+      { width: GROUND_SIZE, height: GROUND_SIZE },
       scene,
     );
     const groundMaterial = new StandardMaterial("groundMaterial", scene);
     groundMaterial.diffuseColor = Color3.FromHexString("#97ae3b");
     ground.material = groundMaterial;
-    ground.physicsImpostor = new PhysicsImpostor(
-      ground,
-      PhysicsImpostor.BoxImpostor,
-      { mass: 0, restitution: 0.9 },
-      scene,
-    );
+
+    // Добавляем физику к полу
+    const groundBody = new Body({
+      mass: 0,
+      position: new Vec3(0, 0, 0),
+    });
+    const groundShape = new Box(new Vec3(GROUND_SIZE / 2, 0, GROUND_SIZE / 2));
+    groundBody.addShape(groundShape);
+    physicsPlugin.world.addBody(groundBody);
+
+    // Обновляем позицию кубов в каждом кадре
+    scene.onBeforeRenderObservable.add(() => {
+      box1.position.set(
+        boxBody1.position.x,
+        boxBody1.position.y,
+        boxBody1.position.z,
+      );
+
+      box2.position.set(
+        boxBody2.position.x,
+        boxBody2.position.y,
+        boxBody2.position.z,
+      );
+
+      ground.position.set(
+        groundBody.position.x,
+        groundBody.position.y,
+        groundBody.position.z,
+      );
+
+      box1.rotation = mapCannonQuaternionToBabylonRotationVector(
+        boxBody1.quaternion,
+      );
+
+      box2.rotation = mapCannonQuaternionToBabylonRotationVector(
+        boxBody2.quaternion,
+      );
+
+      ground.rotation = mapCannonQuaternionToBabylonRotationVector(
+        groundBody.quaternion,
+      );
+
+      // box2.position.set(
+      //   boxBody2.position.x,
+      //   boxBody2.position.y,
+      //   boxBody2.position.z
+      // );
+    });
 
     engine.runRenderLoop(() => {
       scene.render();
