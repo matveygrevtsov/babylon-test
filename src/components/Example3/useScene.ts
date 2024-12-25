b8bc4fed47c2873440a16242c0603752c636db7c
matveygrevtsov <matveygrevcov@gmail.com>
2024-12-25 20:56:48 +0300

    update impostors example

--- src/components/Example3/useScene.ts
+++ src/components/Example3/useScene.ts
@@ -9,20 +9,9 @@ import {
   Color3,
   ArcRotateCamera,
   CannonJSPlugin,
+  PhysicsViewer,
+  PhysicsImpostor,
 } from "@babylonjs/core";
-import { Body, Box, Quaternion, Vec3 } from "cannon-es";
-
-const mapCannonQuaternionToBabylonRotationVector = (
-  quaternion: Quaternion
-): Vector3 => {
-  const { x, y, z, w } = quaternion;
-
-  const rotationX = Math.atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y));
-  const rotationY = Math.asin(2 * (w * y - z * x));
-  const rotationZ = Math.atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z));
-
-  return new Vector3(rotationX, rotationY, rotationZ);
-};
 
 const CUBE_SIZE = 1;
 const GROUND_SIZE = 6;
@@ -72,38 +61,22 @@ export const useScene = () => {
     light.intensity = 0.7;
     // Built-in 'sphere' shape.
 
-    // Создаем куб№1
-    const box1 = MeshBuilder.CreateBox("box", { size: CUBE_SIZE }, scene);
-
-    // Добавляем физику к кубу№1
-    const boxBody1 = new Body({
-      mass: 1,
-      position: new Vec3(0, 6, 0),
-      fixedRotation: true,
-    });
-    const boxShape1 = new Box(
-      new Vec3(CUBE_SIZE / 2, CUBE_SIZE / 2, CUBE_SIZE / 2)
+    // Создаем куб
+    const box = MeshBuilder.CreateBox(
+      "box",
+      {
+        size: CUBE_SIZE,
+      },
+      scene
     );
-    boxBody1.addShape(boxShape1);
-    physicsPlugin.world.addBody(boxBody1);
-
-    // Создаем куб№2
-    const box2 = MeshBuilder.CreateBox("box", { size: CUBE_SIZE }, scene);
-
-    // Добавляем физику к кубу№2
-    const boxBody2 = new Body({
-      mass: 0,
-      position: new Vec3(
-        (0.75 * CUBE_SIZE) / 2,
-        CUBE_SIZE / 2,
-        0.75 * CUBE_SIZE
-      ),
-    });
-    const boxShape2 = new Box(
-      new Vec3(CUBE_SIZE / 2, CUBE_SIZE / 2, CUBE_SIZE / 2)
+    const boxPhysicsImpostor = new PhysicsImpostor(
+      box,
+      PhysicsImpostor.BoxImpostor,
+      { mass: 1 },
+      scene
     );
-    boxBody2.addShape(boxShape2);
-    physicsPlugin.world.addBody(boxBody2);
+    box.physicsImpostor = boxPhysicsImpostor;
+    box.position.y = 5;
 
     // Пол (земля).
     const ground = MeshBuilder.CreateGround(
@@ -114,54 +87,19 @@ export const useScene = () => {
     const groundMaterial = new StandardMaterial("groundMaterial", scene);
     groundMaterial.diffuseColor = Color3.FromHexString("#97ae3b");
     ground.material = groundMaterial;
+    const groundPhysicsImpostor = new PhysicsImpostor(
+      ground,
+      PhysicsImpostor.BoxImpostor,
+      {
+        mass: 0,
+      },
+      scene
+    );
+    ground.physicsImpostor = groundPhysicsImpostor;
 
-    // Добавляем физику к полу
-    const groundBody = new Body({
-      mass: 0,
-      position: new Vec3(0, 0, 0),
-    });
-    const groundShape = new Box(new Vec3(GROUND_SIZE / 2, 0, GROUND_SIZE / 2));
-    groundBody.addShape(groundShape);
-    physicsPlugin.world.addBody(groundBody);
-
-    // Обновляем позицию кубов в каждом кадре
-    scene.onBeforeRenderObservable.add(() => {
-      box1.position.set(
-        boxBody1.position.x,
-        boxBody1.position.y,
-        boxBody1.position.z
-      );
-
-      box2.position.set(
-        boxBody2.position.x,
-        boxBody2.position.y,
-        boxBody2.position.z
-      );
-
-      ground.position.set(
-        groundBody.position.x,
-        groundBody.position.y,
-        groundBody.position.z
-      );
-
-      box1.rotation = mapCannonQuaternionToBabylonRotationVector(
-        boxBody1.quaternion
-      );
-
-      box2.rotation = mapCannonQuaternionToBabylonRotationVector(
-        boxBody2.quaternion
-      );
-
-      ground.rotation = mapCannonQuaternionToBabylonRotationVector(
-        groundBody.quaternion
-      );
-
-      // box2.position.set(
-      //   boxBody2.position.x,
-      //   boxBody2.position.y,
-      //   boxBody2.position.z
-      // );
-    });
+    const physicsViewer = new PhysicsViewer();
+    physicsViewer.showImpostor(boxPhysicsImpostor);
+    physicsViewer.showImpostor(groundPhysicsImpostor);
 
     engine.runRenderLoop(() => {
       scene.render();
