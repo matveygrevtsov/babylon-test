7650f562d3af94c88e4ac5e9dbd828e57ba08938
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2025-01-11 15:32:10 +0300

    fix gravity (#44)
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/components/Example2/Character/constants.ts
+++ src/components/Example2/Character/constants.ts
@@ -5,6 +5,8 @@ export const CAPSULE_RADIUS = 0.6;
 
 export const START_POSITION = new Vector3(0, CAPSULE_HEIGHT / 2, -2);
 
-export const CHARACTER_GRAVITY = new Vector3(0, -18, 0);
+export const GRAVITATIONAL_ACCELERATION = 9.8;
+
+export const CHARACTER_GRAVITY = new Vector3(0, -GRAVITATIONAL_ACCELERATION, 0);
 
 export const SPEED = 5;
